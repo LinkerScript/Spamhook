3b32e293a1a856166cd649f9a1b069f34c66b650
8M <min@effobe.com>
2020-12-29 22:42:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from colorama import init, Fore
+import os
+import requests
+
+class Initialize():
+	init()  # Initialize colorama
+
+
+class __main__(): 
+	"Spamhook's main class. DO NOT MESS WITH ANYTHING IN HERE!!!!!!!!!"
+	
+	
